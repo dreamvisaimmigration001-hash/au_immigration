8899a4a09d7c85c11fae4171e7438d1ea3c24921
DrAmanJy <dramanjy0770@gmail.com>
2026-08-20 13:09:35 +0530

    Remove country dropdown and list

--- src/pages/VevoFirstParty.jsx
+++ src/pages/VevoFirstParty.jsx
@@ -3,9 +3,6 @@ import html2pdf from 'html2pdf.js';
 import './vevo.css';
 import './ess.css';
 
-const COUNTRIES = [
-  "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
-];
 
 function VevoFirstParty() {
   const [visaData, setVisaData] = useState(null);
@@ -170,25 +167,7 @@ function VevoFirstParty() {
                                   </div>
                                 </div>
 
-                                <div id="_2a0a2a0a2e0c" className="wc-field">
-                                  <label htmlFor="_2a0a2a0a2e0c1a_input" id="_2a0a2a0a2e0c0" className="wc-label wc_req">Country of document*</label>
-                                  <div className="wc-input">
-                                    <div id="_2a0a2a0a2e0c1" className="wc-panel">
-                                      <div className="wc-flowlayout wc-hgap-med wc-align-left">
-                                        <div className="wc-cell">
-                                          <span id="_2a0a2a0a2e0c1a" className="wc-dropdown wc-input-wrapper">
-                                            <select id="_2a0a2a0a2e0c1a_input" name="_2a0a2a0a2e0c1a" required defaultValue="">
-                                              <option className="wc-option" value="">Country</option>
-                                              {COUNTRIES.map(country => (
-                                                <option key={country} className="wc-option" value={country}>{country}</option>
-                                              ))}
-                                            </select>
-                                          </span>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
+
                               </div>
                             </div>
                           </div>
